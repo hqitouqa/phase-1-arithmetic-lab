658a855977a4e15018adf7da512e27283349a124
Hala Qitouqa <h.qitouqa@gmail.com>
2023-09-23 20:44:23 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+let num1=1, num2=62;
+let multiply = num1 * num2;
+let random;
+random = Math.floor(Math.random()*10)+1;
+
+let num3=19, num4=5
+let mod = num3 % num4;
+let max=Math.max(2,5,20);
